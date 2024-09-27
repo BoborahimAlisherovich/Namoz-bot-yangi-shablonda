a02e9a6abf711e9aa9ea95b7dc7bbc468db57fcb
Boborahim <boborahimrustamqulov0@gmail.com>
2024-09-27 21:14:19 +0500

    Namoz bot

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -5,4 +5,10 @@ from aiogram.filters import Command
 #help commands
 @dp.message(Command("help"))
 async def help_commands(message:Message):
-    await message.answer("Sizga qanday yordam kerak")
+    await message.answer("""   
+Salom!  Ushbu bot orqali namoz vaqtlarini, namozni qanday o‘qishni, va boshqa muhim ma'lumotlarni topishingiz mumkin.:
+Namoz Vaqtlari: O'z hududingizda namoz vaqtlarini bilib oling. Faqatgina joyingizni kiriting.
+Namoz O‘qish Qoidalari: Har bir namozni qanday o‘qish kerakligini o‘rganing. Har bir namozning qoidalari, ruknlar va tilovatlarini toping.
+Duolar: Har bir namoz uchun tavsiya etilgan duolarni o‘rganing. 
+                         
+""")
